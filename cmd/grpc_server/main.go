2a51799a1f94de9e35fce4923b2c6cbc56e6fe0e
antoneka <ku-9a@mail.ru>
2024-02-18 22:12:09 +0300

    feat: empty implementations of end-points

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -1,11 +1,73 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
 
-	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	desc "github.com/antoneka/auth/pkg/user_v1"
 )
 
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedUserV1Server
+}
+
+func (s *server) Create(
+	ctx context.Context,
+	req *desc.CreateRequest,
+) (*desc.CreateResponse, error) {
+	fmt.Printf("%+v\n", req)
+
+	return &desc.CreateResponse{}, nil
+}
+
+func (s *server) Get(
+	ctx context.Context,
+	req *desc.GetRequest,
+) (*desc.GetResponse, error) {
+	fmt.Printf("+%v\n", req)
+
+	return &desc.GetResponse{}, nil
+}
+
+func (s *server) Update(
+	ctx context.Context,
+	req *desc.UpdateRequest,
+) (*emptypb.Empty, error) {
+	fmt.Printf("+%v\n", req)
+
+	return &emptypb.Empty{}, nil
+}
+
+func (s *server) Delete(
+	ctx context.Context,
+	req *desc.DeleteRequest,
+) (*emptypb.Empty, error) {
+	fmt.Printf("%+v\n", req)
+
+	return &emptypb.Empty{}, nil
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterUserV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
